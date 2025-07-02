715c6b6b5f0397621ac2e5309983496b3374fdc9
RinaWarp Developer <rinawarp@developer.com>
2025-07-02 04:04:44 -0600

    fix: convert Jest config from ES6 modules to CommonJS
    - Change export default to module.exports for Node.js compatibility
    - Fixes Jest configuration loading error in release workflow

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@
  *
  * Project repository: https://github.com/rinawarp/terminal
  */
-export default {
+module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   testMatch: ['<rootDir>/tests/**/*.test.js', '<rootDir>/tests/**/*.spec.js'],
